80e44c136b49238231351da53e0c1f175b830bd0
lewis728 <lewis@aurumgrowthagency.com>
2026-05-29 14:49:18 +0000

    fix: remove standalone output (re-introduced in 626e2d7), update DB to pgbouncer

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,5 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  output: 'standalone',
   webpack: (config) => {
     config.externals = [...(config.externals ?? []), 'pg-native'];
     return config;
